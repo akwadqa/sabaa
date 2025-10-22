d50813f7c000a8f4dbf6abfd7c547f92f03b14b3
Malaz Shukri <malazshukri.contactme@gmail.com>
2025-10-22 16:39:10 +0300

    Added a new custom field to the Expense Entry Detail table and included it in the print format.

---
+++ sabaa/patches/add_expense_date_field_to_expense_entry_detail.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe import _
+
+def execute():
+    custom_fields = {
+        "Expense Entry Detail": [
+            {
+                "fieldname": "expense_date",
+                "label": _("Expense Date"),
+                "fieldtype": "Date",
+                "insert_after": "amount",
+                "module": "Sabaa",
+                "in_list_view": 1,
+                "reqd": 1,
+                "in_standard_filter": 1,
+            }
+        ],
+    }
+
+    create_custom_fields(custom_fields, ignore_validate=True)
+    frappe.db.commit()
