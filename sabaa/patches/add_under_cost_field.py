22cdb7d10aa331b6f56f89e71b308b3d451d62dc
MalazShukri <abuda.b80.ab@gmail.com>
2025-11-17 14:45:30 +0300

    Make the under cost field read only.

--- sabaa/patches/add_under_cost_field.py
+++ sabaa/patches/add_under_cost_field.py
@@ -11,6 +11,7 @@ def execute():
                 "fieldtype": "Check",
                 "insert_after": "total_qty",
                 "module": "Sabaa",
+                "read_only": 1,
             }
         ]
     }
