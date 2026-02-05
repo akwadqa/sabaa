e3591ef6c07e1aaeac55f936da884a910e94acb2
Neamah <neamah@akwad.qa>
2026-02-05 07:46:30 +0000

    fix: resolve make_sales_invoice to include args parameter and improve error logging.

--- sabaa/override/delivery_note_override.py
+++ sabaa/override/delivery_note_override.py
@@ -3,14 +3,14 @@ from frappe import _
 from erpnext.stock.doctype.delivery_note.delivery_note import make_sales_invoice as original_make_sales_invoice
 
 @frappe.whitelist()
-def make_sales_invoice(source_name, target_doc=None):
+def make_sales_invoice(source_name, target_doc=None, args=None):
     try:
         frappe.set_user("Administrator")
 
         delivery_note = frappe.get_doc("Delivery Note", source_name)
         delivery_note.flags.ignore_permissions = True
 
-        sales_invoice = original_make_sales_invoice(source_name, target_doc)
+        sales_invoice = original_make_sales_invoice(source_name, target_doc, args)
 
         sales_invoice.driver = delivery_note.driver
         sales_invoice.driver_name = delivery_note.driver_name
@@ -18,5 +18,5 @@ def make_sales_invoice(source_name, target_doc=None):
         return sales_invoice
 
     except Exception:
-        frappe.log_error(frappe.get_traceback(), "Custom make_sales_invoice Error")
+        frappe.log_error("Custom make_sales_invoice Error", frappe.get_traceback())
         frappe.throw(_("Error while creating Sales Invoice. Check logs."))
